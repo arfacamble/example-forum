69d39d724f6dc936e9cdfedf6f0bad8482160284
Arthur Campbell <arfa.camble@gmail.com>
2021-07-02 14:43:52 +0100

    all sorts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class='col-full'>
+    <h1>The Forum: People Talk</h1>
+    <ThreadList :threads="threads"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import sourceData from '../data'
+import ThreadList from '../components/ThreadList'
 export default {
-  name: 'Home',
   components: {
-    HelloWorld
+    ThreadList
+  },
+
+  data() {
+    return {
+      threads: Object.values(sourceData.threads),
+      posts: sourceData.posts,
+      users: sourceData.users
+    }
   }
 }
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
